5fed984383ab0af47e35716eea43eeca212c7a06
loghmari sami <106961988+loghmari@users.noreply.github.com>
2022-11-21 10:13:32 +0100

    Add files via upload

---
+++ exam03/exam_printf.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exam_printf.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sloghmar <sloghmar@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 11:57:55 by sloghmar          #+#    #+#             */
+/*   Updated: 2022/11/08 16:05:09 by sloghmar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void	put_str(char *str, int *len)
+{
+	if (!str)
+		str = "(null)";
+	while (*str)
+		*len += write(1, str++, 1);
+}
+
+void	put_digit(long long int nbr, int base, int *len)
+{
+	char	*hexa;
+
+	hexa = "0123456789abcdef";
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		*len += write(1, "-", 1);
+	}
+	if (nbr >= base)
+		put_digit((nbr / base), base, len);
+	*len += write(1, &hexa[nbr % base], 1);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int			len;
+	va_list		ptr;
+
+	len = 0;
+	va_start(ptr, format);
+	while (*format)
+	{
+		if ((*format == '%') && *(format + 1))
+		{
+			format++;
+			if (*format == 's')
+				put_str(va_arg(ptr, char *), &len);
+			else if (*format == 'd')
+				put_digit((long long int)va_arg(ptr, int), 10, &len);
+			else if (*format == 'x')
+				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
+		}
+		else
+			len += write(1, format, 1);
+		format++;
+	}
+	return (va_end(ptr), len);
+}
+#include <limits.h>
+
+int main()
+
+{
+	printf("%x\n", 1566545);
+	ft_printf("%x", 1566545);
+
+	return (0);
+}
+
